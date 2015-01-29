c21ae40f6e949b4a27ac7cf511ac2b4174af49a7
Mauricio nunes <mauricioskateboard@gmail.com>
2015-01-29 17:46:27 -0200

    code started

--- page.js
+++ page.js
@@ -1,62 +1,112 @@
-$(function() {
-    var client = new WindowsAzure.MobileServiceClient('https://mcvclicenciamento.azure-mobile.net/', 'GfnDwsbCakIOvLUBpvCIXaVIgdIgrr58'),
-        todoItemTable = client.getTable('todoitem');
-
-    // Read current data and rebuild UI.
-    // If you plan to generate complex UIs like this, consider using a JavaScript templating library.
-    function refreshTodoItems() {
-        var query = todoItemTable.where({ complete: false });
-
-        query.read().then(function(todoItems) {
-            var listItems = $.map(todoItems, function(item) {
-                return $('<li>')
-                    .attr('data-todoitem-id', item.id)
-                    .append($('<button class="item-delete">Delete</button>'))
-                    .append($('<input type="checkbox" class="item-complete">').prop('checked', item.complete))
-                    .append($('<div>').append($('<input class="item-text">').val(item.text)));
-            });
-
-            $('#todo-items').empty().append(listItems).toggle(listItems.length > 0);
-            $('#summary').html('<strong>' + todoItems.length + '</strong> item(s)');
-        }, handleError);
-    }
-
-    function handleError(error) {
-        var text = error + (error.request ? ' - ' + error.request.status : '');
-        $('#errorlog').append($('<li>').text(text));
-    }
-
-    function getTodoItemId(formElement) {
-        return $(formElement).closest('li').attr('data-todoitem-id');
-    }
-
-    // Handle insert
-    $('#add-item').submit(function(evt) {
-        var textbox = $('#new-item-text'),
-            itemText = textbox.val();
-        if (itemText !== '') {
-            todoItemTable.insert({ text: itemText, complete: false }).then(refreshTodoItems, handleError);
+(function(){
+    angular.module('app',['ui.bootstrap'])
+    .factory('azureConfig', [function () {
+        var client = new WindowsAzure.MobileServiceClient('https://mcvclicenciamento.azure-mobile.net/', 'GfnDwsbCakIOvLUBpvCIXaVIgdIgrr58') 
+        var get = function(table,query){
+            var table = client.getTable(table)
+            if(query)return table.read(query);
+            else return table.read(query);
         }
-        textbox.val('').focus();
-        evt.preventDefault();
-    });
+        var insert = function(table,item){
+            var table = client.getTable(table)
+            return table.insert(item)
+        }
+        var del = function (table,item){
+            var table = client.getTable(table)
+            return table.del({id:item.id})
+        }
+        var update = function(table,item){
 
-    // Handle update
-    $(document.body).on('change', '.item-text', function() {
-        var newText = $(this).val();
-        todoItemTable.update({ id: getTodoItemId(this), text: newText }).then(null, handleError);
-    });
+        }
+        return {
+            client:client,
+            get:get,
+            insert:insert,
+            update:update,
+            del:del
+        };
+    }])
+    .directive('listItems', [function () {
+        return {
+            restrict: 'EA',
+            require:['?table'],
+            link:function(scope,element,attr){
+                scope.table = attr.table
+                scope.load()
+            },
+            controller:function($scope,azureConfig){
+                $scope.load = function(){
+                    $scope.get($scope.table).then(function(items){
+                        $scope.$apply(function(){
+                            $scope.items = items
+                        })
+                    })
+                }
+                $scope.get = azureConfig.get;
+              $scope.remove = function(item){
+                    azureConfig.del($scope.table,item).then($scope.load)
+              }
+              $scope.$on('refresh-items',$scope.load)
+            },
+            template:'<div class="">'+
+                        '<h3>{{table}}</h3>'+
+                        '<alert ng-repeat="item in items" type="success" close="remove(item)">{{item.name}}</alert>'+
+                        '<alert data-ng-if="!items.length" type="info">Nenhum registro</alert>'+
+                    '</div>'
+        };
+    }])
+    .directive('listItemsForm', [function () {
+        return {
+            restrict: 'EA',
+            require:['?models','?typeModels','?table'],
+            link:function(scope,element,attr){
+                scope.models = attr.models.split(',')
+                scope.typeModels = attr.typeModels.split(',')
+                scope.table = attr.table
+            },
+            controller:function($scope,azureConfig){
+                $scope.item = {}
+                $scope.reset = function(form) {
+                    if (form) {
+                      form.$setPristine();
+                      form.$setUntouched();
+                    }
+                };
+                $scope.insert = function(item){
+                    azureConfig.insert($scope.table,item).then(function(item){
+                        $scope.$emit('refresh-items',item)
+                        $scope.item = {}
+                    })
+                }
+            },
+            template:'<form name="form" novalidate class="col-md-12">'+
+                        '<div class="form-group" data-ng-repeat="model in models">'+
+                           '<label class="control-label" for="{{model}}">{{model}}</label>'+
+                           '<div class="controls class="col-md-12"">'+
+                                '<input placeholder="{{model}}" data-ng-model="item[models[$index]]" type="{{typeModels[$index]}}"/>'+
+                           '</div>'+
+                        '</div>'+
+                        '<div ng-if="!form.$valid">'+
+                           'Há algo errado com o formulario'+
+                        '</div>'+
+                        '<input type="button" class="btn btn-primary" ng-click="reset(form)" value="Reset" />'+
+                        '<input type="submit" data-ng-if="form.$valid && form.$dirty" class="btn btn-primary" ng-click="insert(item)" value="Save" />'+
+                     '</form>'
+        };
+    }])
+    .directive("loadingIndicator", function () {
+        return {
+            restrict: "A",
+            template: '<progressbar class="progress-striped active" value="100" type="info">Loading....</progressbar>',
+            link: function (scope, element, attrs) {
+                scope.$on("loading-started", function (e) {
+                    element.css({ "display": "" });
+                });
+                scope.$on("loading-complete", function (e) { 
+                    element.css({ "display": "none" }); 
+                });
 
-    $(document.body).on('change', '.item-complete', function() {
-        var isComplete = $(this).prop('checked');
-        todoItemTable.update({ id: getTodoItemId(this), complete: isComplete }).then(refreshTodoItems, handleError);
-    });
-
-    // Handle delete
-    $(document.body).on('click', '.item-delete', function () {
-        todoItemTable.del({ id: getTodoItemId(this) }).then(refreshTodoItems, handleError);
-    });
-
-    // On initial load, start by fetching the current data
-    refreshTodoItems();
-});
+            }
+        }})
+    .config(function ($httpProvider) {$httpProvider.interceptors.push(function ($q, $rootScope) {return {'request': function (config) {$rootScope.$broadcast('loading-started');return config || $q.when(config);},'response': function (response) {$rootScope.$broadcast('loading-complete');return response || $q.when(response)}}})});
+})()
